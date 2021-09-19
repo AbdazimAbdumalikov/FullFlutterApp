75d884f9a1516e0b12c446e8db83059538a9109d
AbdazimAbdumalikov <80280105+AbdazimAbdumalikov@users.noreply.github.com>
2021-09-19 14:35:20 +0600

    WOH!

---
+++ flutter_1/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_1/pages/home.dart';
+import 'package:flutter_1/pages/main_screen.dart';
+
+
+
+void main() => runApp(MaterialApp(
+  theme: ThemeData(
+    primaryColor: Colors.deepOrangeAccent,
+  ),
+  initialRoute: '/',
+  routes: {
+    '/': (context) => MainScreen(),
+    '/todo': (context) => Home(),
+  },
+));
